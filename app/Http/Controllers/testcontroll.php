d4dd3777ff86736359801fd0755932ca2265857e
zach0627 <s1216999@gmail.com>
2018-11-27 00:43:46 +0800

    first commit

---
+++ app/Http/Controllers/testcontroll.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testcontroll extends Controller
+{
+    //
+    public function index()
+    {
+     echo("123");
+     
+    }
+}
